37f9d4762bba691e8976dfcc6590d75b33d4cec6
Lambdav <103351988+Lambdav@users.noreply.github.com>
2026-03-11 18:52:34 +0100

    corrected ex08

--- piscine/shell01/ex08/add_chelou.sh
+++ piscine/shell01/ex08/add_chelou.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-NBR1=$(echo $FT_NBR1 | tr "\'\\\\\"\?\!" "01234")
-NBR2=$(echo $FT_NBR2 | tr "mrdoc" "01234")
-echo "ibase=5; obase=13; $NBR1 + $NBR2" | bc | tr "0123456789ABC" "gtaio luSnemf"
+NBR1=$(echo "$FT_NBR1" | tr "'\\\\\"?!" "01234")
+NBR2=$(echo "$FT_NBR2" | tr "mrdoc" "01234")
+echo "obase=13; ibase=5; $NBR1 + $NBR2" | bc | tr "0123456789ABC" "gtaio luSnemf"
