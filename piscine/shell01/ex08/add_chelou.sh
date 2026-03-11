977e9f3d2535cbf6b18a78a258bc6614cbc1ed51
Lambdav <103351988+Lambdav@users.noreply.github.com>
2026-03-11 10:54:20 +0100

    added ex08

---
+++ piscine/shell01/ex08/add_chelou.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+NBR1=$(echo $FT_NBR1 | tr "\'\\\\\"\?\!" "01234")
+NBR2=$(echo $FT_NBR2 | tr "mrdoc" "01234")
+echo "ibase=5; obase=13; $NBR1 + $NBR2" | bc | tr "0123456789ABC" "gtaio luSnemf"
