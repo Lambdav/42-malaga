774f85dde11a07bbdc859fca7791e2b264a983e6
Lambdav <103351988+Lambdav@users.noreply.github.com>
2026-03-12 13:58:25 +0100

    added commented out main
    Removed commented-out code for main function and includes.

--- piscine/c01/ex02/ft_swap.c
+++ piscine/c01/ex02/ft_swap.c
@@ -6,3 +6,20 @@ void  ft_swap(int *a, int *b)
 	*a = *b;
 	*b = temp;
 }
+
+/*
+#include <stdio.h>
+
+void	ft_swap(int *a, int *b);
+
+int	main(void)
+{
+	int	a = 10;
+	int	b = 20;
+
+	printf("Antes: a=%d, b=%d\n", a, b);
+	ft_swap(&a, &b);
+	printf("Despues: a=%d, b=%d\n", a, b);
+	return (0);
+}
+*/
