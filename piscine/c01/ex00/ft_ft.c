f902e4dced0a7b622bd8b4d5be507d7501e1c475
Lambdav <103351988+Lambdav@users.noreply.github.com>
2026-03-12 13:56:48 +0100

    Clean up ft_ft.c by removing comments
    Removed commented code and updated printf statement.

--- piscine/c01/ex00/ft_ft.c
+++ piscine/c01/ex00/ft_ft.c
@@ -6,16 +6,14 @@ void  ft_ft(int *nbr)
 /*
 #include <stdio.h>
 void	ft_ft(int *nbr);
-void	ft_ultimate_ft(int *********nbr);
 
 int	main(void)
 {
 	int	n;
 
-	// Prueba Ex 00
 	n = 0;
 	ft_ft(&n);
-	printf("Ex00 (esperado 42): %d\n", n);
+	printf("%d\n", n);
 	return (0);
 }
 */
