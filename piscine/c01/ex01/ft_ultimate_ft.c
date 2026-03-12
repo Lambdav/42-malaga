2158adb2fb44375e8c6ef354b3cfefa034d9d909
Lambdav <103351988+Lambdav@users.noreply.github.com>
2026-03-12 14:01:11 +0100

    added commented out main

--- piscine/c01/ex01/ft_ultimate_ft.c
+++ piscine/c01/ex01/ft_ultimate_ft.c
@@ -6,25 +6,24 @@ void  ft_ultimate_ft(int *********nbr)
 /*
 #include <stdio.h>
 
-// Prototipos para que el main los reconozca
 void	ft_ultimate_ft(int *********nbr);
 
 int	main(void)
 {
 	int	n;
+	int	*p1 = &n;
+	int	**p2 = &p1;
+	int	***p3 = &p2;
+	int	****p4 = &p3;
+	int	*****p5 = &p4;
+	int	******p6 = &p5;
+	int	*******p7 = &p6;
+	int	********p8 = &p7;
+	int	*********p9 = &p8;
 
-	// Prueba Ex 01
-	int *p1 = &n;
-	int **p2 = &p1;
-	int ***p3 = &p2;
-	int ****p4 = &p3;
-	int *****p5 = &p4;
-	int ******p6 = &p5;
-	int *******p7 = &p6;
-	int ********p8 = &p7;
 	n = 0;
-	ft_ultimate_ft(&p8);
-	printf("Ex01 (esperado 42): %d\n", n);
+	ft_ultimate_ft(p9);
+	printf("Resultado ft_ultimate_ft: %d (esperado: 42)\n", n);
 	return (0);
 }
 */
