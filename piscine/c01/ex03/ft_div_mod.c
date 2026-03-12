3c0de412c940797756835b492814687630572bd5
Lambdav <103351988+Lambdav@users.noreply.github.com>
2026-03-12 14:02:22 +0100

    added commented out main

--- piscine/c01/ex03/ft_div_mod.c
+++ piscine/c01/ex03/ft_div_mod.c
@@ -8,23 +8,16 @@ void	ft_div_mod(int a, int b, int *div, int *mod)
 #include <stdio.h>
 
 void	ft_div_mod(int a, int b, int *div, int *mod);
-void	ft_ultimate_div_mod(int *a, int *b);
 
 int	main(void)
 {
-	int a = 13;
-	int b = 3;
-	int div, mod;
+	int	a = 13;
+	int	b = 3;
+	int	res_div;
+	int	res_mod;
 
-	// Prueba 03
-	ft_div_mod(a, b, &div, &mod);
-	printf("Ex03: Div=%d, Mod=%d\n", div, mod);
-
-	// Prueba 04
-	int x = 13;
-	int y = 3;
-	ft_ultimate_div_mod(&x, &y);
-	printf("Ex04: x (div)=%d, y (mod)=%d\n", x, y);
+	ft_div_mod(a, b, &res_div, &res_mod);
+	printf("a: 13, b: 3 -> div: %d, mod: %d\n", res_div, res_mod);
 	return (0);
 }
 */
