1ed0c50e2b76b4dd96dfd3e36e93ff9d73ecf10a
Lambdav <103351988+Lambdav@users.noreply.github.com>
2026-03-12 13:56:05 +0100

    added commented out main

--- piscine/c01/ex00/ft_ft.c
+++ piscine/c01/ex00/ft_ft.c
@@ -2,4 +2,20 @@ void  ft_ft(int *nbr)
 {
   *nbr=42;
 }
-  
+
+/*
+#include <stdio.h>
+void	ft_ft(int *nbr);
+void	ft_ultimate_ft(int *********nbr);
+
+int	main(void)
+{
+	int	n;
+
+	// Prueba Ex 00
+	n = 0;
+	ft_ft(&n);
+	printf("Ex00 (esperado 42): %d\n", n);
+	return (0);
+}
+*/
