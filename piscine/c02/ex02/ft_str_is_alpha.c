19f2ecb107fdded255c036ec75dd18b71b28bf58
Lambdav <103351988+Lambdav@users.noreply.github.com>
2026-03-13 09:41:06 +0100

    Update ft_str_is_alpha.c

--- piscine/c02/ex02/ft_str_is_alpha.c
+++ piscine/c02/ex02/ft_str_is_alpha.c
@@ -9,8 +9,10 @@ int	ft_str_is_alpha(char *str)
     while (str[i] != '\0')
     {
     	if (!(str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z'))
-        return (0);
-        i++;
+		{
+        	return (0);
+		}
+		i++;
     }
     return (1);
 }
