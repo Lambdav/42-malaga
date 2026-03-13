30266aa75d61e2d5444444301fcc0478bee5c4e8
Lambdav <103351988+Lambdav@users.noreply.github.com>
2026-03-13 10:12:47 +0100

    Update ft_strupcase.c

--- piscine/c02/ex07/ft_strupcase.c
+++ piscine/c02/ex07/ft_strupcase.c
@@ -7,11 +7,11 @@ char *ft_strupcase(char *str)
     i = 0;
     while (str[i] != '\0')
     {
-    	if (str[i] >= 'a' && (str[i] <= 'z')
+    	if (str[i] >= 'a' && str[i] <= 'z')
         {
         	str[i] -= 32;
-            i++;
         }
+	i++;
     }
     return (str);
 }
