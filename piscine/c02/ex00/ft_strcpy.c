d601fadacf691c52ce0fe5345d7bca4965ce3a7f
Lambdav <103351988+Lambdav@users.noreply.github.com>
2026-03-13 08:53:59 +0100

    Update ft_strcpy.c

--- piscine/c02/ex00/ft_strcpy.c
+++ piscine/c02/ex00/ft_strcpy.c
@@ -1,13 +1,27 @@
-char  *ft_strcpy(char *dest, char *src)
-{
-  int i;
+// #include <stdio.h>
 
-  i = 0;
-  while (src[i] != '\0')
-  {
-    dest[i] = src[i];
-    i++;
-  }
-  dest[i] = '\0';
-  return (dest);
+char    *ft_strcpy(char *dest, char *src)
+{
+	int    i;
+	
+	i = 0;
+	
+	while (src[i] != '\0')
+	{
+	    dest[i] = src[i];
+	    i++;
+	}
+	dest[i] = src[i];
+	return (dest);
 }
+
+/*
+int main(void)
+{
+	char	dest[10] = "Dest";
+	char	src[10] = "Source";
+
+	printf("dest before: %s\n", dest);
+	ft_strcpy(dest, src);
+	printf("dest after: %s\n", dest);
+} */
